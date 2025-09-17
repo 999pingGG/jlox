950b7268dd82dcc235d2c986285c2fba69ae737a
Arnoldo Adonaí Barón Robles <adonai_1111@hotmail.com>
2025-09-16 23:25:39 -0600

    Implement RPN

---
+++ src/me/elinge/lox/Rpn.java
@@ -0,0 +1,51 @@
+package me.elinge.lox;
+
+// Reverse Polish Notation
+class Rpn implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return String.format(
+                "%s %s %s",
+                expr.left.accept(this),
+                expr.right.accept(this),
+                expr.operator.lexeme());
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return expr.expression.accept(this);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        return expr.value == null ? "nil" : expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return String.format(
+                "%s%s",
+                expr.right.accept(this),
+                expr.operator.lexeme());
+    }
+
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+                new Expr.Grouping(
+                        new Expr.Binary(
+                                new Expr.Literal(1),
+                                new Token(TokenType.PLUS, "+", null, 1),
+                                new Expr.Literal(2))),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Binary(
+                                new Expr.Literal(4),
+                                new Token(TokenType.MINUS, "-", null, 1),
+                                new Expr.Literal(3))));
+        System.out.println(new Rpn().print(expression));
+    }
+}
