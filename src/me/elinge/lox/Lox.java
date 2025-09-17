a2a97310baf784a10a493259c3b156bfff6e997d
Arnoldo Adonaí Barón Robles <adonai_1111@hotmail.com>
2025-09-17 17:59:15 -0600

    Implement parser

--- src/me/elinge/lox/Lox.java
+++ src/me/elinge/lox/Lox.java
@@ -48,18 +48,31 @@ public class Lox {
         var scanner = new Scanner(source);
         var tokens = scanner.scanTokens();
 
-        // For now, just print the tokens.
-        for (var token : tokens) {
-            System.out.println(token);
+        var parser = new Parser(tokens);
+        var expression = parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError) {
+            return;
         }
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
-        report(line, message);
+        report(line, "", message);
     }
 
-    private static void report(int line, String message) {
-        System.err.printf("[line %d] Error: %s%n", line, message);
+    private static void report(int line, String where, String message) {
+        System.err.printf("[line %d] Error%s: %s%n", line, where, message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type() == TokenType.EOF) {
+            report(token.line(), " at the end", message);
+        } else {
+            report(token.line(), " at '" + token.lexeme() + "'", message);
+        }
     }
 }
